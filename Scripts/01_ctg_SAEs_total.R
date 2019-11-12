55c8c6f861acf85f76278ae66a4cb739cef076a4
dmcalli2 <david.mcallister@glasgow.ac.uk>
2019-11-12 00:23:09 +0000

    update intervention_arm_relations

--- Scripts/01_ctg_SAEs_total.R
+++ Scripts/01_ctg_SAEs_total.R
@@ -4,6 +4,12 @@ library(tidyverse)
 aact <- readRDS("../Trial_identify/clinical_trials_august_2017/ctg/Output_data/all_aact_tables.Rds")
 load("../Trial_identify/clinical_trials_august_2017/ctg/Output_data/trials_conditions_lookup_atc_code_lookup.Rdata")
 
+## Make nrowreturn function
+nrowR <- function(x) {
+  print(nrow(x))
+  x
+}
+
 #  Read in all trials with drugs that are in older people trials
 drugsearch <- c("telmisartan", "valsartan", "olmesartan", "aliskiren", "irbesartan")
 
@@ -28,19 +34,94 @@ aact <- map(aact, function(x) {
   if("nct_id" %in% names(x)) x %>% filter(nct_id %in% trials$nct_id)
 })
 
-## Examine SAEs
+## Trial treatment comparisons ----
+trial_comparison <- intvn_lbl %>% 
+  select( nct_id, ctg_str_original, ctg_n, ctg_str, type_label, unq_name) %>% 
+  distinct()
+
+## Note complete join of intervention ID to ctg_strings
+trial_comparison <- trial_comparison %>% 
+  inner_join(aact$interventions %>% 
+             rename(ctg_str_original = name) %>% 
+              mutate(ctg_str_original = str_to_lower(ctg_str_original)) %>% 
+               select(nct_id, intervention_id = id, ctg_str_original) %>% 
+               distinct())
+
+trial_comparison2 <- trial_comparison %>% 
+  left_join(aact$design_group_interventions %>% select(-id) %>% distinct()) %>% 
+  left_join(aact$design_groups %>% select(arm_type = group_type, arm_title = title, design_group_id = id) %>% distinct()) %>% 
+  select(nct_id, arm_id = design_group_id, arm_type, arm_title, intervention_id, intvn_text_original = ctg_str_original,  everything()) %>% 
+  rename(intvn_txt_n = ctg_n,  intvn_txt_cmpnt = ctg_str, intvn_txt_lbl = type_label, who_atc_name = unq_name) %>% 
+  arrange(nct_id, arm_id, intervention_id)
+
+
+intvn_arms <- aact$interventions %>% 
+  filter(name %in% trial_comparison$ctg_str_original)
+
+
+arms_known <- arms %>% 
+  mutate(arms_compare = str_sub(arms_compare, 4)) %>% 
+  separate(arms_compare, into = c("arm_a", "arm_b"), sep = "v")
+arms_a <- arms_known %>% 
+  select(nct_id, arm = arm_a, drug = a) 
+arms_b <- arms_known %>% 
+  select(nct_id, arm = arm_b, drug = b) 
+arms_new <- bind_rows(a = arms_a, b = arms_b) %>% 
+  arrange(nct_id, arm, drug) %>% 
+  distinct() %>% 
+  group_by(nct_id, drug) %>% 
+  summarise(probable_arm = paste(arm, collapse = ", ")) %>% 
+  ungroup()
+
+trial_comparison <- trial_comparison %>% 
+  left_join(arms_new %>% rename(unq_name = drug)) %>% 
+  left_join(atc_lbl %>% rename(unq_name = title)) %>% 
+  mutate(code_shrt = str_sub(code, 1, 5)) %>% 
+  left_join(atc_lbl %>% rename(code_shrt = code)) %>% 
+  select(-code_shrt)
+write_csv(trial_comparison, "Data_extraction/01_trial_drug_comparisons.csv")
+
+
+## Primary outcome ----
+primary <- aact$design_outcomes %>% 
+  filter(outcome_type == "primary") %>% 
+  select(nct_id, measure, description) %>% 
+  distinct()
+
+
+## eligibilities ----
+elig <- aact$eligibilities %>% 
+  select(nct_id, minimum_age, maximum_age, gender) %>% 
+  distinct()
+
+## participants for all ----
+participants <- trials %>% 
+  select(nct_id, enrollment)
+
+## Baseline results ----
+baseline <- aact$baseline_measurements %>% 
+  filter(title %in% c("Age", "Age, Customized", "Body Mass Index", "Body Mass Index (BMI)",  "Body Mass Index Class",
+                      "Gender", "Gender, Customized")) %>% 
+  left_join(aact$result_groups %>% select(nct_id, ctgov_group_code, result_type, group_name = title)) %>% 
+  select(nct_id, ctgov_group_code, group_name, everything()) %>% 
+  arrange(nct_id, title, ctgov_group_code)
+
+
+## Examine AEs/SAEs
 sae <- aact$reported_events %>%
-  filter(adverse_event_term == "Total, serious adverse events")
+  filter(adverse_event_term %in% c("Total, serious adverse events", "Total, other adverse events"))
 sae <- sae %>%
-  select(id, nct_id, result_group_id, ctgov_group_code, time_frame,
+  select(id, nct_id, adverse_event_term, result_group_id, ctgov_group_code, time_frame,frequency_threshold,
          default_vocab, subjects_affected, subjects_at_risk) %>%
   as_tibble()
 sae <- sae %>%
-  inner_join(aact$result_groups %>% select(-id)) %>%
-  select(-ctgov_group_code, -result_type, -result_group_id)
+  inner_join(aact$result_groups %>% select(-id) %>% rename(group_name = title)) %>%
+  select(nct_id, group_name, ctgov_group_code, everything()) %>% 
+  arrange(nct_id, ctgov_group_code) 
 
-## Identify trials for which dont have SAEs and add in treatment arms to
-## create lookup table
+## Identify trials for which have results
+
+
 no_sae <- aact$studies %>% 
   anti_join(sae) %>% 
   select(nct_id, phase, source) 
